cee92feef2365f5def1b054f1ab74104a19f872f
wangxiaohui2015 <wxhyzh2005@163.com>
2023-11-18 16:29:26 +0800

    Delte unused files.

--- springboot-properties/src/main/java/com/my/study/properties/config/Configuration.java
+++
@@ -1,9 +0,0 @@
-package com.my.study.properties.config;
-
-import org.springframework.context.annotation.PropertySource;
-
-@org.springframework.context.annotation.Configuration
-@PropertySource("classpath:customized2.properties")
-@PropertySource("classpath:random.properties")
-public class Configuration {
-}
--- springboot-web/src/main/resources/static/js/main.js
+++
@@ -1,3 +0,0 @@
-function delete_employee(id) {
-	alert("Delete employee id:" + id);
-}
